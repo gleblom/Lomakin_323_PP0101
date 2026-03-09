7eb1f719d39adfad366ef446c3370e0c271eb669
gleblomakin70 <gleblomakin70@gmail.com>
2026-03-09 21:13:52 +0400

    global usings

--- DocumentManagemnetService/GlobalUsings/GlobalUsings.cs
+++ DocumentManagemnetService/GlobalUsings/GlobalUsings.cs
@@ -5,4 +5,10 @@ global using UserControl = System.Windows.Controls.UserControl;
 global using Application = System.Windows.Application;
 global using MessageBox = System.Windows.MessageBox;
 global using Button = System.Windows.Controls.Button;
+global using Brushes = System.Windows.Media.Brushes;
+global using TextBox = System.Windows.Controls.TextBox;
+global using DataObject = System.Windows.DataObject;
+global using DataFormats = System.Windows.DataFormats;
+global using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+global using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
 
