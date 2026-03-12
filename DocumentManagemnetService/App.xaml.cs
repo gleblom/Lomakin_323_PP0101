cc4fd3bab958531b8c94b1f762bb898780af6132
gleblomakin70 <gleblomakin70@gmail.com>
2026-03-12 15:25:33 +0400

    updater delete

--- DocumentManagemnetService/App.xaml.cs
+++ DocumentManagemnetService/App.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using DocumentManagementService;
 using DocumentManagementService.Models;
-using DocumentManagementService.UpdateChecker;
 using DocumentManagementService.Views;
 using Microsoft.Extensions.Configuration;
 using NLog;
@@ -81,7 +80,6 @@ namespace DocumentManagemnetService
         {
             try
             {
-                await new UpdateChecker().CheckAndUpdateAsync();
 
                 var builder = new ConfigurationBuilder()
                     .AddEnvironmentVariables()
